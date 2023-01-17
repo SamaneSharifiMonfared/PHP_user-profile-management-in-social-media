8967c84876311eab904c7e02134cbefa5c6dee97
Samane Sharifi Monfared <samane.sharify@gmail.com>
2023-01-17 18:23:05 +0000

    Update connection.php

--- functions/connection.php
+++ functions/connection.php
@@ -8,7 +8,7 @@ class sqlConnection{
 //    Mysql configuration -> will need to go to the configure file if I get the time :D
         $dbhost = 'localhost';
         $dbuser = 'root';
-        $dbpass = 'sami1net';
+        $dbpass = '*********';
         $db =  'socialmedia';
 //connection
         $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
